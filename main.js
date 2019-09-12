28bcc6e090f5f0360cbf53355311ce00108f2c04
Kayla Wood <kaylaewood@gmail.com>
2019-09-12 13:45:47 -0600

    Insert capitalized name into rules page

--- main.js
+++ main.js
@@ -5,11 +5,32 @@ var rulesScreen = document.querySelector('.rules-screen');
 // buttons
 var startPlayButton = document.querySelector('#start-play-button');
 
-// event listeners
-startPlayButton.addEventListener('click', switchSections);
+// inputs
+var startPlayer1Input = document.querySelector('#start-player1-input');
+var startPlayer2Input = document.querySelector('#start-player2-input');
 
-// functions
-function switchSections() {
-  startScreen.classList.add('hide');
-  rulesScreen.classList.remove('hide');
+// text changes
+var rulesPlayer1Name = document.querySelector('#rules-player1-name');
+var rulesPlayer2Name = document.querySelector('#rules-player2-name');
+
+
+// event listeners
+startPlayButton.addEventListener('click', clickStartPlayButton);
+
+// mega functions
+function clickStartPlayButton() {
+  switchSections(startScreen, rulesScreen);
+  insertNames(rulesPlayer1Name, startPlayer1Input);
+  insertNames(rulesPlayer2Name, startPlayer2Input);
+};
+
+//functions
+function switchSections(hide, show) {
+  hide.classList.add('hide');
+  show.classList.remove('hide');
+};
+
+function insertNames(text, input) {
+  var capitalName = input.value.toUpperCase();
+  text.innerHTML = capitalName;
 };
