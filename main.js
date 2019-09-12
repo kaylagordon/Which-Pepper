58c461fd5f267a71c7f0db61c61502428413f28e
Kayla Wood <kaylaewood@gmail.com>
2019-09-12 15:26:35 -0600

    Style game board screen

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // sections
 var startScreen = document.querySelector('.start-screen');
 var rulesScreen = document.querySelector('.rules-screen');
+var gameScreen = document.querySelector('.game-screen');
+var gameOverScreen = document.querySelector('.game-over-screen');
 
 // buttons
 var startPlayButton = document.querySelector('#start-play-button');
+var rulesPlayButton = document.querySelector('#rules-play-button');
 
 // inputs
 var startPlayer1Input = document.querySelector('#start-player1-input');
@@ -13,16 +16,27 @@ var startPlayer2Input = document.querySelector('#start-player2-input');
 var rulesPlayer1Name = document.querySelector('#rules-player1-name');
 var rulesPlayer2Name = document.querySelector('#rules-player2-name');
 
+// other variables
+var startErrorMessage = document.querySelector('.start-error-message');
 
 // event listeners
 startPlayButton.addEventListener('click', clickStartPlayButton);
+rulesPlayButton.addEventListener('click', clickRulesPlayButton);
 
 // mega functions
 function clickStartPlayButton() {
-  switchSections(startScreen, rulesScreen);
-  insertNames(rulesPlayer1Name, startPlayer1Input);
-  insertNames(rulesPlayer2Name, startPlayer2Input);
+  if (startPlayer1Input.value.length) {
+    switchSections(startScreen, rulesScreen);
+    insertNames(rulesPlayer1Name, startPlayer1Input);
+    insertNames(rulesPlayer2Name, startPlayer2Input);
+  } else {
+    showErrorMessage(startErrorMessage);
+  }
 };
+
+function clickRulesPlayButton() {
+  switchSections(rulesScreen, gameScreen)
+}
 
 //functions
 function switchSections(hide, show) {
@@ -34,3 +48,7 @@ function insertNames(text, input) {
   var capitalName = input.value.toUpperCase();
   text.innerHTML = capitalName;
 };
+
+function showErrorMessage(errorText) {
+  errorText.classList.remove('hide');
+}
